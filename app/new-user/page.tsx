3809ff039826fafe8e918e15887dbc19da98da18
Evan Thomas <evanthomas0292@gmail.com>
2023-09-14 14:00:46 +0100

    Add NewUser server component to add new users to planetscale database

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,6 +1,33 @@
-const NewUser = () => {
+import { prisma } from "@/utils/db"
+import { currentUser } from "@clerk/nextjs"
+import { redirect } from 'next/navigation'
+
+const createNewUser = async () => {
+  const user = await currentUser()
+  console.log(user)
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string,
+    }
+  })
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user?.emailAddresses[0].emailAddress
+      }
+    })
+  }
+  redirect('/diary')
+}
+
+
+const NewUser = async () => {
+  await createNewUser()
   return (
-    <div>NewUser</div>
+    <div>...loading</div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Link from 'next/link';
+import { auth } from '@clerk/nextjs';
 
-const Home = () => {
+const Home = async () => {
+  const { userId } = await auth()
+  const href = userId ? '/diary' : '/new-user'
+
   return (
     <div className='flex justify-center items-center w-screen h-screen'>
       <div className='max-w-4xl'>
         <h1 className='text-8xl mb-4'>Plant Points Tracker</h1>
         <p className='text-2xl mb-12'>Keep track of the variety of plant based foods you eat. Each ingredient is worth a point. Aim for 30 per week.</p>
-          <Link className='bg-green-300 rounded-md px-4 py-2 text-lg' href="/home">Get Started</Link>
+          <Link className='bg-green-300 rounded-md px-4 py-2 text-lg' href={href}>Get Started</Link>
       </div>
     </div>
   )
