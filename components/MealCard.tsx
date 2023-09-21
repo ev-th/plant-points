321e6c457314c6ad545ed82ca2df5dd097a8c30e
Evan Thomas <evanthomas0292@gmail.com>
2023-09-21 15:21:10 +0100

    Meal Card shows points total

--- components/MealCard.tsx
+++ components/MealCard.tsx
@@ -1,13 +1,17 @@
 const MealCard = ({ meal }) => {
   const date = new Date(meal.eatenAt).toDateString()
+  const points = meal.ingredients.reduce((sum, ingredient) => sum + +ingredient.points, 0)
 
     return (
       <div className="bg-green-300 rounded-lg p-2 my-1">
         <h4 className="text-xl font-semibold">{meal.name}</h4>
-        <div>Eaten on {date}</div>
-        {meal.ingredients.map(ingredient => (
-          <div key={meal.id}>- {ingredient.name}</div>)
-        )}
+        <p>Eaten on {date}</p>
+        <p>Points: {points}</p>
+        <ul>
+          {meal.ingredients.map(ingredient => (
+            <li key={meal.id}>- {ingredient.name}</li>)
+          )}
+        </ul>
       </div>
     )
   }
