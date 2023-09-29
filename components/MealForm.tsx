91ea12719efd69bb6089f851bfe5e41e29b46768
Evan Thomas <evanthomas0292@gmail.com>
2023-09-29 14:32:42 +0100

    Refactor MealForm to use react-hook-form

--- components/MealForm.tsx
+++ components/MealForm.tsx
@@ -7,6 +7,8 @@ import "react-datepicker/dist/react-datepicker.css";
 import { createNewMeal } from '@/utils/api';
 import { useRouter } from 'next/navigation';
 
+import { useForm, useController, Controller } from 'react-hook-form'
+
 
 const MealForm = ({ ingredientOptions }) => {
   const [loading, setLoading] = useState(false)
@@ -15,52 +17,95 @@ const MealForm = ({ ingredientOptions }) => {
     ingredients: [],
     date: new Date()
   })
-  
+  const { register, control, handleSubmit, formState: {errors} } = useForm({
+    defaultValues: {
+      name: "",
+      ingredients: [],
+      date: new Date()
+    }
+  })
+
+  // const { field } = useController({ name: "ingredients", control })
   const router = useRouter()
 
   const ingredientsSelectOptions = ingredientOptions.map((ingredient) => {
     return { value: ingredient.id, label: ingredient.name}
   })
 
-  const submitForm = async (e) => {
-    e.preventDefault()
+  console.log(errors)
+
+  const submitForm = async (formValues) => {
+    // e.preventDefault()
     setLoading(true)
-    const { name, ingredients, date } = formData
+    console.log("ingredients", formValues.ingredients)
 
     await createNewMeal({
-      name,
-      ingredientIds: ingredients.map(ingredient => ingredient.value),
-      date
+      name: formValues.name,
+      ingredientIds: formValues.ingredients.map(ingredient => ingredient.value),
+      date: formValues.date
     })
     setLoading(false)
+    console.log("formData111111111", formData)
+    console.log("formValues", formValues)
 
     router.push('/diary')
+  }
+
+
+
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setFormData(prev => ({...prev, [name]: value}))
+  }
+  const handleIngredientsChange = (option) => {
+    console.log("option", option)
+    field.onChange(option)
+    // setFormData(prev => ({...prev, ingredients: option}))
+  }
+  const handleDateChange = (option) => {
+    setFormData(prev => ({...prev, date: option}))
   }
 
   return (
     <div>
       {loading && <div>Loading...</div>}
-      <form className="p-4" onSubmit={submitForm}>
-        <DatePicker
-          selected={formData.date}
-          onChange={value => setFormData(prev => ({...prev, date: value}))}
+      <form className="p-4" onSubmit={handleSubmit(submitForm)}>
+        <Controller
+          name="date"
+          control={control}
+          render={({ field }) => (
+            <DatePicker
+              {...field}
+              selected={field.value}
+            />
+
+          )}
+        
         />
 
         <input
-          name="meal"
           type="text"
           placeholder="Meal Name"
-          value={formData.name}
-          onChange={e => setFormData(prev => ({...prev, name: e.target.value}))}
+          {...register("name", { required: 'This is required', minLength: 4 })}
+        />
+        <Controller
+          name="ingredients"
+          control={control}
+          render={({ field }) => (
+            <Select
+              {...field}
+              isMulti={true}
+              options={ingredientsSelectOptions}
+              closeMenuOnSelect={false}
+              // value={field.value}
+              // onChange={handleIngredientsChange}
+            />
+
+          )}
+        
         />
 
-        <Select
-          onChange={value => setFormData(prev => ({...prev, ingredients: value}))}
-          isMulti={true}
-          options={ingredientsSelectOptions}
-          closeMenuOnSelect={false}
-        />
-        
         <input type="submit" />
       </form>
     </div>
