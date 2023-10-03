84e56b99beab443b1312843b8e893416b7bcdb02
Evan Thomas <evanthomas0292@gmail.com>
2023-10-03 22:51:12 +0100

    Add error catching to meal POST request handler

--- app/api/meal/route.ts
+++ app/api/meal/route.ts
@@ -1,21 +1,36 @@
 import { getUserByClerkId } from "@/utils/auth"
 import { prisma } from "@/utils/db"
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
+import { Prisma } from "@prisma/client"
 
-export const POST = async (request: Request) => {
+export const POST = async (request: NextRequest ) => {
   const user = await getUserByClerkId()
-  const { name, ingredientIds, date } = await request.json()
 
-  const meal = await prisma.meal.create({
-    data: {
-      userId: user.id,
-      name,
-      eatenAt: date,
-      ingredients: {
-        connect: ingredientIds.map((id: String) => ({id: id}))
+  if(!user) {
+    return NextResponse.json({ error: 'Auth Error' }, { status: 401 })
+  }
+
+  try {
+    const { name, ingredientIds, date } = await request.json()
+  
+    const meal = await prisma.meal.create({
+      data: {
+        userId: user.id,
+        name,
+        eatenAt: date,
+        ingredients: {
+          connect: ingredientIds.map((id: String) => ({id: id}))
+        }
       }
-    }
-  })
+    })
 
-  return NextResponse.json({ data: meal })
+    return NextResponse.json({ data: meal })
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      console.log(error.message)
+      
+      return NextResponse.json({ error: 'Database Error' }, { status: 500 })
+    }
+  }
+
 }
