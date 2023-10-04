11008d6608239d95bd5d7c18191504fe75ba216c
Evan Thomas <evanthomas0292@gmail.com>
2023-10-04 16:53:19 +0100

    Add error handling to all route handlers

--- app/api/meal/[id]/route.ts
+++ app/api/meal/[id]/route.ts
@@ -1,42 +1,76 @@
 import { getUserByClerkId } from "@/utils/auth"
 import { NextResponse } from "next/server"
 import { prisma } from "@/utils/db"
+import { Prisma } from "@prisma/client"
 
-export const PATCH = async (request, { params }) => {
-  const { name, ingredientIds, date } = await request.json()
 
+export const PATCH = async (request: NextResponse, { params }: { params: {id: string } }) => {
   const user = await getUserByClerkId()
 
-  const updatedRecipe = await prisma.meal.update({
-    where: {
-      userId_id: {
-        userId: user.id,
-        id: params.id
-      }
-    },
-    data: {
-      name,
-      eatenAt: date,
-      ingredients: {
-        set: ingredientIds.map(id => ({id: id}))
-      }
-    }
-  })
+  if (!user) {
+    return NextResponse.json({ error: 'User not found' }, { status: 401 })
+  }
 
-  return NextResponse.json({ data: updatedRecipe })
+  try {
+    const { name, ingredientIds, date } = await request.json()
+
+    const updatedMeal = await prisma.meal.update({
+      where: {
+        userId_id: {
+          userId: user.id,
+          id: params.id
+        }
+      },
+      data: {
+        name,
+        eatenAt: date,
+        ingredients: {
+          set: ingredientIds.map((id: string) => ({id: id}))
+        }
+      }
+    })
+
+    return NextResponse.json({ data: updatedMeal })
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      console.error(`Prisma error: ${error.message}`)
+
+      return NextResponse.json({ error: `Database Error: ${error.message}` }, { status: 500 })
+    } else {
+      console.error(`Unexpected error: ${error}`)
+
+      return NextResponse.json({ error: 'Internal server error' }, { status: 500 })
+    }
+  }
 }
 
-export const DELETE = async (request, { params }) => {
+export const DELETE = async (request: NextResponse, { params }: { params: { id: string } }) => {
   const user = await getUserByClerkId()
-  
-  const deletedRecipe = await prisma.meal.delete({
-    where: {
-      userId_id: {
-        userId: user.id,
-        id: params.id
-      }
-    }
-  })
 
-  return NextResponse.json({ data: deletedRecipe })
+  if (!user) {
+    return NextResponse.json({ error: 'User not found' }, { status: 401 })
+  }
+  
+  try {
+    const deletedRecipe = await prisma.meal.delete({
+      where: {
+        userId_id: {
+          userId: user.id,
+          id: params.id
+        }
+      }
+    })
+  
+    return NextResponse.json({ data: deletedRecipe })
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      console.error(`Prisma error: ${error.message}`)
+
+      return NextResponse.json({ error: `Database Error: ${error.message}` }, { status: 500 })
+    } else {
+      console.error(`Unexpected error: ${error}`)
+
+      return NextResponse.json({ error: 'Internal server error' }, { status: 500 })
+    }
+  }
 }
--- app/api/meal/route.ts
+++ app/api/meal/route.ts
@@ -3,11 +3,11 @@ import { prisma } from "@/utils/db"
 import { NextRequest, NextResponse } from "next/server"
 import { Prisma } from "@prisma/client"
 
-export const POST = async (request: NextRequest ) => {
+export const POST = async (request: NextRequest) => {
   const user = await getUserByClerkId()
 
   if(!user) {
-    return NextResponse.json({ error: 'Auth Error' }, { status: 401 })
+    return NextResponse.json({ error: 'User not found' }, { status: 401 })
   }
 
   try {
@@ -27,10 +27,13 @@ export const POST = async (request: NextRequest ) => {
     return NextResponse.json({ data: meal })
   } catch (error) {
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log(error.message)
+      console.error(`Prisma error: ${error.message}`)
+
+      return NextResponse.json({ error: `Database Error: ${error.message}` }, { status: 500 })
+    } else {
+      console.error(`Unexpected error: ${error}`)
       
-      return NextResponse.json({ error: 'Database Error' }, { status: 500 })
+      return NextResponse.json({ error: 'Internal server error' }, { status: 500 })
     }
   }
-
 }
