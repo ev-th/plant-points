123c7ed80a821cb80eeea21e771841e6b8c5bfe6
Evan Thomas <evanthomas0292@gmail.com>
2023-10-08 12:37:05 +0100

    Fix path alias bug in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   test: {
+    alias: {
+      '@/': new URL('./', import.meta.url).pathname,
+    },
     include: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
     globals: true,
     environment: 'jsdom',
