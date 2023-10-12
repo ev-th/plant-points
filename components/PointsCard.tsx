27f9bee510a61431761f7ecd8e3e5705471db0fe
Evan Thomas <evanthomas0292@gmail.com>
2023-10-12 12:56:47 +0100

    Write tests for PointsCard

--- components/PointsCard.tsx
+++ components/PointsCard.tsx
@@ -1,14 +1,14 @@
 import { calculatePoints, getUniqueIngredients } from '@/utils/points'
 import { MealWithIngredients } from '@/utils/types'
 
-const PointsCard = async ({meals}: {meals: MealWithIngredients[]}) => {
+const PointsCard = ({meals}: {meals: MealWithIngredients[]}) => {
   const ingredients = getUniqueIngredients(meals)
   const points = calculatePoints(meals)
   return (
     <div className='bg-gray-300 rounded-lg p-2'>
-      <p>Your plant points over the last 7 days: {points}</p>
+      <p role='region'>Your plant points over the last 7 days: {points}</p>
       <p className='font-semibold'>Ingredients you&#39;ve eaten this week:</p>
-      <ul>
+      <ul role="list">
         {ingredients.map(ingredient => (
           <li key={ingredient.id}>- {ingredient.name}, points: {ingredient.points.toString()}</li>)
         )}
