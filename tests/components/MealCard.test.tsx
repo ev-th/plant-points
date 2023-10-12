6956f201b3668c7bbe420f4f7cf306ad007eb69a
Evan Thomas <evanthomas0292@gmail.com>
2023-10-12 11:36:25 +0100

    Write MealCard test

---
+++ tests/components/MealCard.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react'
+import MealCard from '@/components/MealCard'
+import { MealWithIngredients } from '@/utils/types'
+
+describe(MealCard, () => {
+  const meal = {
+    id: 'testId',
+    name: 'testName',
+    eatenAt: new Date('2023-10-12'),
+    ingredients: [
+      {id: 'ingredientId1', name: 'testIngredient1', points: 1},
+      {id: 'ingredientId2', name: 'testIngredient2', points: 2},
+      {id: 'ingredientId3', name: 'testIngredient3', points: 0.25},
+    ],
+  } as MealWithIngredients
+
+  
+  test('displays the meal name', () => {
+    render(MealCard({ meal }))
+    expect(screen.getByText('testName')).toBeInTheDocument()
+  })
+  
+  test('displays the date eaten', () => {
+    render(MealCard({ meal }))
+    expect(screen.getByText('Eaten on Thu Oct 12 2023')).toBeInTheDocument()
+  })
+  
+  test('displays the total plant points', () => {
+    render(MealCard({ meal }))
+    expect(screen.getByText('Points: 3.25')).toBeInTheDocument()
+  })
+  
+  test('displays the ingredients', () => {
+    render(MealCard({ meal }))
+    expect(screen.getByText('- testIngredient1')).toBeInTheDocument()
+    expect(screen.getByText('- testIngredient2')).toBeInTheDocument()
+    expect(screen.getByText('- testIngredient3')).toBeInTheDocument()
+  })
+})
