f3483d3fde972bb98dd6fbacff530279a34267ea
Evan Thomas <evanthomas0292@gmail.com>
2023-10-13 16:49:54 +0100

    Add tests for MealPage

--- tests/app/MealPage.test.tsx
+++ tests/app/MealPage.test.tsx
@@ -2,12 +2,40 @@ import { render, screen } from '@testing-library/react'
 import { vi } from 'vitest'
 import MealPage from '@/app/(dashboard)/meals/[id]/page'
 
-vi.mock('@/utils/auth', () => {
+const mocks = vi.hoisted(() => {
   return {
-    getUserByClerkId: () => 'testClerkId'
+    getMealWithIngredients: vi.fn(),
+    getIngredients: vi.fn()
+  }
+})
+
+vi.mock('@/components/MealForm', () => {
+  return {
+    default: ({meal, ingredientOptions}: {meal: string, ingredientOptions: string}) => <div>meal: {meal}, ingredientOptions: {ingredientOptions}</div>
+  }
+})
+vi.mock("@/utils/dbQueries", () => {
+  return {
+    getMealWithIngredients: mocks.getMealWithIngredients,
+    getIngredients: mocks.getIngredients,
   }
 })
 
 describe(MealPage, () => {
-  test.todo('meal page tests')
+  test('passes down a meal and ingredient options to the MealPage component', async () => {
+    mocks.getMealWithIngredients.mockReturnValueOnce('testMeal')
+    mocks.getIngredients.mockReturnValueOnce('testIngredients')
+    render( await MealPage({params: {id: 'testId'}}))
+    expect(screen.getByText('meal: testMeal, ingredientOptions: testIngredients')).toBeInTheDocument()
+  })
+  
+  test('calls meal with params.id', async () => {
+    render( await MealPage({params: {id: 'testId'}}))
+    expect(mocks.getMealWithIngredients).toHaveBeenCalledWith('testId')
+  })
+  
+  test('calls getIngredients', async () => {
+    render( await MealPage({params: {id: 'testId'}}))
+    expect(mocks.getIngredients).toHaveBeenCalledOnce()
+  })
 })
