46000fbb9c18d20aa1a9c4acb53ec5a47d9ffa54
Evan Thomas <evanthomas0292@gmail.com>
2023-11-06 14:02:41 +0000

    Add format Date function

---
+++ tests/utils/formatDate.test.ts
@@ -0,0 +1,15 @@
+import {formatDate} from '@/utils/formatDate'
+
+describe(formatDate, () => {
+  test('it returns "1 Jan" when passed date object of that date', () => {
+    const date = new Date('2023-01-01')
+    const formattedDate = formatDate(date)
+    expect(formattedDate).toBe('1 Jan')
+  })
+  
+  test('it returns "31 Dec" when passed date object of that date', () => {
+    const date = new Date('2023-12-31')
+    const formattedDate = formatDate(date)
+    expect(formattedDate).toBe('31 Dec')
+  })
+})
---
+++ utils/formatDate.ts
@@ -0,0 +1,10 @@
+export const formatDate = (date: Date): string => {
+  const months = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+  ]
+  const monthIndex = date.getMonth()
+  const month = months[monthIndex]
+  const day = date.getDate()
+  return `${day} ${month}`
+}
