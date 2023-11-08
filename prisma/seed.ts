52117bffe49e2b73414449a370289fa1ace9df01
Evan Thomas <evanthomas0292@gmail.com>
2023-11-08 16:26:25 +0000

    Add ingredients seed file

---
+++ prisma/seed.ts
@@ -0,0 +1,19 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const ingredientData = require("./ingredients.json");
+
+async function main() {
+  const ingredients = await prisma.ingredient.createMany({
+    data: ingredientData,
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
